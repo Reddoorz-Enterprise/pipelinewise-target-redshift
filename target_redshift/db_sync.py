982130483081aecc5a70b955bccadd85ded1e923
atif <atif.imam@reddoorz.com>
2022-05-04 17:15:59 +0530

    fix versioning issue

--- target_redshift/db_sync.py
+++ target_redshift/db_sync.py
@@ -860,7 +860,7 @@ class DbSync:
 
         for column in columns_to_add:
             self.add_column(column, stream)
-
+        filtered_column_list = [column_info for column_info in columns_dict if 'character varying' in list(column_info.values())]
         columns_to_replace = [
             (safe_column_name(name), column_clause(name, properties_schema))
             for (name, properties_schema) in self.flatten_schema.items()
